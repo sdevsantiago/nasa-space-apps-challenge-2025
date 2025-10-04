fe200bd27ffe5d94cbd1387d335a69f6761802b4
marcnava <marcnava@student.42madrid.com>
2025-10-04 14:39:14 +0200

    Updated textures and camera control

---
+++ shelter43/Assets/Code/Scripts/DragObject.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+    
+    Vector3 _startPosition;
+    Vector3 _mousePosition;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
